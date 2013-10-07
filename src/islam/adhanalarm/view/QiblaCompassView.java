496636739ad9272e61f7b0e9d55b3898916c9609
Shuhrat Dehkanov <k@efir.uz>
2013-10-08 00:09:33 +0900

    Lint check: do not instantiate new objects in loops

--- src/islam/adhanalarm/view/QiblaCompassView.java
+++ src/islam/adhanalarm/view/QiblaCompassView.java
@@ -1,8 +1,7 @@
 package islam.adhanalarm.view;
 
-import java.text.DecimalFormat;
-
 import uz.efir.muazzin.R;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +11,8 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class QiblaCompassView extends View {
     private float directionNorth = 0;
@@ -28,15 +29,18 @@ public class QiblaCompassView extends View {
     private int height = 240;
     private float centre_x = width * 0.5f;
     private float centre_y = height * 0.5f;
+    private Paint paint = new Paint();
 
     public QiblaCompassView(Context context) {
         super(context);
         initCompassView();
     }
+
     public QiblaCompassView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         initCompassView();
     }
+
     public QiblaCompassView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initCompassView();
@@ -49,21 +53,25 @@ public class QiblaCompassView extends View {
 
     private void initCompassView() {
         compassNeedle = BitmapFactory.decodeResource(getResources(), R.drawable.compass_needle);
-        compassBackground = BitmapFactory.decodeResource(getResources(), R.drawable.compass_background);
+        compassBackground = BitmapFactory.decodeResource(getResources(),
+                R.drawable.compass_background);
         width = getWidth(compassBackground, 240);
         height = getHeight(compassBackground, 240);
-        centre_x = width  * 0.5f;
+        centre_x = width * 0.5f;
         centre_y = height * 0.5f;
-        rotateNeedle.postTranslate(centre_x - getWidth(compassNeedle, 23) + 10, centre_y - getHeight(compassNeedle, 142) + 10);
+        rotateNeedle.postTranslate(centre_x - getWidth(compassNeedle, 23) + 10, centre_y
+                - getHeight(compassNeedle, 142) + 10);
         invalidate();
     }
 
-    public void setConstants(TextView bearingNorth, CharSequence bearingNorthString, TextView bearingQibla, CharSequence bearingQiblaString) {
+    public void setConstants(TextView bearingNorth, CharSequence bearingNorthString,
+            TextView bearingQibla, CharSequence bearingQiblaString) {
         this.bearingNorth = bearingNorth;
         this.bearingNorthString = bearingNorthString.toString();
         this.bearingQibla = bearingQibla;
         this.bearingQiblaString = bearingQiblaString.toString();
-        compassBackground = BitmapFactory.decodeResource(getResources(), R.drawable.compass_background);
+        compassBackground = BitmapFactory.decodeResource(getResources(),
+                R.drawable.compass_background);
         compassNeedle = BitmapFactory.decodeResource(getResources(), R.drawable.compass_needle);
     }
 
@@ -71,20 +79,23 @@ public class QiblaCompassView extends View {
         this.directionNorth = directionNorth;
         this.directionQibla = directionQibla;
         rotateNeedle = new Matrix();
-        rotateNeedle.postRotate(-directionQibla, getWidth(compassNeedle, 23) * 0.5f, getHeight(compassNeedle, 142));
-        rotateNeedle.postTranslate(centre_x - getWidth(compassNeedle, 23), centre_y - getHeight(compassNeedle, 142) + 5);
+        rotateNeedle.postRotate(-directionQibla, getWidth(compassNeedle, 23) * 0.5f,
+                getHeight(compassNeedle, 142));
+        rotateNeedle.postTranslate(centre_x - getWidth(compassNeedle, 23),
+                centre_y - getHeight(compassNeedle, 142) + 5);
         invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         bearingNorth.setText(bearingNorthString.replace("(1)", df.format(directionNorth)));
-        bearingQibla.setText(bearingQiblaString.replace("(+/-)", directionQibla >= 0 ? " +" : " -").replace("(2)", df.format(Math.abs(directionQibla))).replace("(3)",  df.format(directionNorth + directionQibla)));
+        bearingQibla.setText(bearingQiblaString.replace("(+/-)", directionQibla >= 0 ? " +" : " -")
+                .replace("(2)", df.format(Math.abs(directionQibla)))
+                .replace("(3)", df.format(directionNorth + directionQibla)));
 
-        Paint p = new Paint();
         canvas.rotate(-directionNorth, centre_x, centre_y);
-        canvas.drawBitmap(compassBackground, 0, 0, p);
-        canvas.drawBitmap(compassNeedle, rotateNeedle, p);
+        canvas.drawBitmap(compassBackground, 0, 0, paint);
+        canvas.drawBitmap(compassNeedle, rotateNeedle, paint);
     }
 
     private int getHeight(Bitmap bitmap, int def) {
