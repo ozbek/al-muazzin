18ac6050a19a310733bfd701dffc99ff1dbc8cb7
Shuhrat Dehkanov <k@efir.uz>
2015-03-05 21:17:57 +0900

    Recreate the intent if it is null

--- app/src/main/java/islam/adhanalarm/service/StartNotificationService.java
+++ app/src/main/java/islam/adhanalarm/service/StartNotificationService.java
@@ -7,9 +7,12 @@ import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.util.Calendar;
+
 import islam.adhanalarm.CONSTANT;
 import islam.adhanalarm.Notifier;
 import islam.adhanalarm.Preferences;
+import islam.adhanalarm.Schedule;
 import islam.adhanalarm.WakeLock;
 import islam.adhanalarm.receiver.StartNotificationReceiver;
 import uz.efir.muazzin.Muazzin;
@@ -26,62 +29,67 @@ public class StartNotificationService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        /**
-         * We do the actual work in a separate thread since a Service has a limited life and we want to guarantee completion
-         */
-        final class StartNotificationTask implements Runnable {
+        if (intent == null) {
+            Log.d("NotificationService", "The intent is null, recreate and shot a new one");
+            Schedule today = Schedule.today(this);
+            short nextTimeIndex = today.nextTimeIndex();
+            Calendar actualTime = today.getTimes()[nextTimeIndex];
+            intent = new Intent();
+            intent.putExtra(CONSTANT.EXTRA_ACTUAL_TIME, actualTime.getTimeInMillis());
+            intent.putExtra(CONSTANT.EXTRA_TIME_INDEX, nextTimeIndex);
+        }
+        new Thread(new StartNotificationTask(this, intent)).start();
 
-            private final Context context;
-            private final Intent intent;
+        return START_REDELIVER_INTENT;
+    }
 
-            public StartNotificationTask(Context c, Intent i) {
-                context = c;
-                intent = i;
+    /**
+     * We do the actual work in a separate thread since a Service has a limited life and we want to guarantee completion
+     */
+    final class StartNotificationTask implements Runnable {
+
+        private final Context context;
+        private final Intent intent;
+
+        public StartNotificationTask(Context c, Intent i) {
+            context = c;
+            intent = i;
+        }
+
+        @Override
+        public void run() {
+            if (Utils.getIsForeground()) {
+                // Update the UI marker and set the notification for the next prayer
+                Intent i = new Intent(context, Muazzin.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            } else {
+                StartNotificationReceiver.setNext(context);
             }
-
-            @Override
-            public void run() {
-                if (Utils.getIsForeground()) {
-                    // Update the UI marker and set the notification for the next prayer
-                    Intent i = new Intent(context, Muazzin.class);
-                    i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(i);
-                } else {
-                    StartNotificationReceiver.setNext(context);
-                }
 
 //                Utils.updateWidgets(context);
 
-                short timeIndex = intent.getShortExtra(CONSTANT.EXTRA_TIME_INDEX, (short) -1);
-                if (timeIndex == -1) { // Got here from boot
-                    Preferences preferences = Preferences.getInstance(context);
-                    if (preferences.getBasmalaEnabled()) {
-                        MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.bismillah);
-                        mediaPlayer.setScreenOnWhilePlaying(true);
-                        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                            @Override
-                            public void onCompletion(MediaPlayer mp) {
-                                WakeLock.release();
-                            }
-                        });
-                        mediaPlayer.start();
-                    } else {
-                        WakeLock.release();
-                    }
+            short timeIndex = intent.getShortExtra(CONSTANT.EXTRA_TIME_INDEX, (short) -1);
+            if (timeIndex == -1) { // Got here from boot
+                Preferences preferences = Preferences.getInstance(context);
+                if (preferences.getBasmalaEnabled()) {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.bismillah);
+                    mediaPlayer.setScreenOnWhilePlaying(true);
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            WakeLock.release();
+                        }
+                    });
+                    mediaPlayer.start();
                 } else {
-                    // Notify the user for the current time, need to do this last since it releases the WakeLock
-                    long actualTime = intent.getLongExtra(CONSTANT.EXTRA_ACTUAL_TIME, 0);
-                    Notifier.start(context, timeIndex, actualTime);
+                    WakeLock.release();
                 }
+            } else {
+                // Notify the user for the current time, need to do this last since it releases the WakeLock
+                long actualTime = intent.getLongExtra(CONSTANT.EXTRA_ACTUAL_TIME, 0);
+                Notifier.start(context, timeIndex, actualTime);
             }
         }
-
-        if (intent != null) {
-            new Thread(new StartNotificationTask(this, intent)).start();
-        } else {
-            Log.wtf("StartNotificationService", "The intent was null");
-        }
-
-        return START_REDELIVER_INTENT;
     }
 }
