b77d5b50d4f296763487240dc1ffb8a7a38dfdba
Shuhrat Dehkanov <k@efir.uz>
2015-03-06 21:18:12 +0900

    We want the notifications be delivered in time
    From developer docs:
    Beginning with API 19 (KITKAT) alarm delivery is inexact: the OS will shift
    alarms in order to minimize wakeups and battery use. There are new APIs to
    support applications which need strict delivery guarantees;
    see setWindow(int, long, long, PendingIntent) and
    setExact(int, long, PendingIntent).
    Applications whose targetSdkVersion is earlier than API 19 will continue to see
    the previous behavior in which all alarms are delivered exactly when requested.

--- app/src/main/java/islam/adhanalarm/receiver/StartNotificationReceiver.java
+++ app/src/main/java/islam/adhanalarm/receiver/StartNotificationReceiver.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import java.util.Calendar;
 
@@ -31,7 +32,13 @@ public class StartNotificationReceiver extends BroadcastReceiver {
         intent.putExtra(CONSTANT.EXTRA_TIME_INDEX, timeIndex);
 
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        am.set(AlarmManager.RTC_WAKEUP, actualTime.getTimeInMillis(), PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT));
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            am.setExact(AlarmManager.RTC_WAKEUP, actualTime.getTimeInMillis(),
+                    PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT));
+        } else {
+            am.set(AlarmManager.RTC_WAKEUP, actualTime.getTimeInMillis(),
+                    PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT));
+        }
     }
 
     @Override
